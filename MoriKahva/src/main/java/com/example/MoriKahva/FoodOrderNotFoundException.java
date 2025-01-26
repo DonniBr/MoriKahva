d020c7295f43028ba855dfabe52dd73b00aa36b0
DonniBr <donniebr@tutamail.com>
2025-01-26 22:12:37 +0100

    Delete MoriKahva directory

--- MoriKahva/src/main/java/com/example/MoriKahva/FoodOrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.MoriKahva;
-
-public class FoodOrderNotFoundException extends RuntimeException{
-    FoodOrderNotFoundException(Long id){
-        super("Could not find order" + id);
-    }
-}
--- MoriKahva/src/main/java/com/example/MoriKahva/FoodOrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.MoriKahva;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodOrderRepository  extends JpaRepository <FoodOrder, Long> {
-}
